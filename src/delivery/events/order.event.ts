384abb0050a1dea20ee599adf4c376b94a6a3037
mrjared16 <doannguyentanphuc99@gmail.com>
2021-06-16 18:07:35 +0700

    update order event payload

--- src/delivery/events/order.event.ts
+++ src/delivery/events/order.event.ts
@@ -3,24 +3,31 @@ import { OrderStatus } from 'src/shared/enums/order-status.enum';
 
 export class OrderEventPayload {
   id: string;
-  cashierId: string;
-  restaurantId: string;
-  createdAt: Date;
-  updatedAt: Date;
-  status: OrderStatus;
+  // cashierId?: string;
+  // restaurantId?: string;
+  // createdAt?: Date;
+  // updatedAt?: Date;
+  // status?: OrderStatus;
   delivery: DeliveryPayload;
+  subTotal: number;
+  grandTotal: number;
+  invoice: {
+    payment: {
+      method: 'COD' | 'PAYPAL' | 'CASH';
+    };
+  };
 }
 
 export class DeliveryPayload {
-  customerId: string;
-  driverId: string;
-  customerAddress: string;
-  customerGeom: { type: string; coordinates: number[] };
-  restaurantAddress: string;
-  restaurantGeom: { type: string; coordinates: number[] };
+  // customerId?: string;
+  // driverId?: string;
+  // customerAddress: string;
+  // customerGeom: { type: string; coordinates: number[] };
+  // restaurantAddress: string;
+  restaurantGeom?: { type: string; coordinates: number[] };
   distance: number;
   shippingFee: number;
-  status: DeliveryStatus;
-  createdAt: Date;
-  updatedAt: Date;
+  // status: DeliveryStatus;
+  // createdAt: Date;
+  // updatedAt: Date;
 }
