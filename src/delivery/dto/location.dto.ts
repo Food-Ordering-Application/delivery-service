3bcf1d6fbdb9cdc51e7d16b35bcb62621ab1f545
mrjared16 <doannguyentanphuc99@gmail.com>
2021-06-16 18:07:18 +0700

    add get distance and get nearby location

--- src/delivery/dto/location.dto.ts
+++ src/delivery/dto/location.dto.ts
@@ -1,4 +1,4 @@
-import { S2Cell, S2CellId, S2LatLng } from 'nodes2ts';
+import { S2Cell, S2CellId, S2LatLng, S2RegionCoverer, Utils } from 'nodes2ts';
 const LEVEL = 12;
 export class Location {
   latitude: number;
@@ -59,5 +59,47 @@ export class Location {
 
   static toS2LatLong({ latitude, longitude }: Location): S2LatLng {
     return S2LatLng.fromDegrees(latitude, longitude);
+  }
+
+  static getGeoHashesNearLocation(
+    location: Location,
+    radius: number,
+  ): string[] {
+    const region = Utils.calcRegionFromCenterRadius(
+      Location.toS2LatLong(location),
+      radius,
+      8,
+    );
+
+    const regionCoverer = new S2RegionCoverer();
+    // regionCoverer.setMaxCells(10);
+    regionCoverer.setMaxLevel(LEVEL);
+    regionCoverer.setMinLevel(LEVEL);
+
+    const cells = regionCoverer.getCoveringCells(region);
+    const results = cells.map((r) => r.toToken());
+
+    console.log(results.join(','));
+
+    return results;
+  }
+
+  static degreeToRadian(deg: number): number {
+    return deg * (Math.PI / 180);
+  }
+
+  static getDistanceFrom2Location(point1: Location, point2: Location): number {
+    const R = 6371; // Radius of the earth in kilometers
+    const dLat = Location.degreeToRadian(point2.latitude - point1.latitude); // Location.degreeToRadian below
+    const dLon = Location.degreeToRadian(point2.longitude - point1.longitude);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(Location.degreeToRadian(point1.latitude)) *
+        Math.cos(Location.degreeToRadian(point2.latitude)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c * 1000; // Distance in KM
+    return d;
   }
 }
